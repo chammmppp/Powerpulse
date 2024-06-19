69f4de64714034273eb1aba007dd8de3fdcb28d3
Thichanon Sompetch <champ.thichanon@gmail.com>
2024-06-19 19:13:40 +0700

    first commit

---
+++ category/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+
+
+# Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+    slug = models.SlugField(max_length=100, unique=True)
+    description = models.TextField()
+    image = models.ImageField(upload_to='photo/categories', blank=True)
+
+    class Meta:
+        verbose_name = 'category'
+        verbose_name_plural = 'categories'
+
+    def __str__(self):
+        return self.name
